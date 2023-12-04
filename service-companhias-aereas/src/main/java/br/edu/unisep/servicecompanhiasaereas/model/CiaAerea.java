420346d6b215e650c766566f84a7d2554a6d2326
Nicolas <nicolasgfe3@gmail.com>
2023-12-04 19:33:29 -0300

    create service, dtos, crud, repository and controller.

---
+++ service-companhias-aereas/src/main/java/br/edu/unisep/servicecompanhiasaereas/model/CiaAerea.java
@@ -0,0 +1,20 @@
+package br.edu.unisep.servicecompanhiasaereas.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "ciaAerea")
+@Getter
+@Setter
+@Entity
+public class CiaAerea {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+    private String nome;
+}
