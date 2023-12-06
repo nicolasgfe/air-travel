f74fc4b69aaeabfe998e00d701e1fad400747502
Nicolas <nicolasgfe3@gmail.com>
2023-12-05 21:42:02 -0300

    update repository aeroporto, create model, repository and controlle with aviao.

--- service-companhias-aereas/src/main/java/br/edu/unisep/servicecompanhiasaereas/model/Aviao.java
+++ service-companhias-aereas/src/main/java/br/edu/unisep/servicecompanhiasaereas/model/Aviao.java
@@ -1,20 +1,21 @@
 package br.edu.unisep.servicecompanhiasaereas.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "aeroporto")
+@Table(name = "aviao")
 @Getter
 @Setter
 @Entity
-public class Aeroporto {
+public class Aviao {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
     private int codigo;
-    private String cidade;
-
+    private int qtdassentos;
 }
