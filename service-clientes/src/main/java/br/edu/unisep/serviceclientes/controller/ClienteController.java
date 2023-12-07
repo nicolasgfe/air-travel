025b5231c3daf75fc4832c4d90623b070751d1ae
Guilherme De Souza <souzadegui@gmail.com>
2023-12-07 14:54:23 -0300

    Create Crud Client

--- service-clientes/src/main/java/br/edu/unisep/serviceclientes/controller/ClienteController.java
+++ service-clientes/src/main/java/br/edu/unisep/serviceclientes/controller/ClienteController.java
@@ -5,24 +5,70 @@ import br.edu.unisep.serviceclientes.model.Client;
 import br.edu.unisep.serviceclientes.repository.ClientRepository;
 import br.edu.unisep.serviceclientes.service.ClientService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("api/cliente")
 @RequiredArgsConstructor
 @RestController
 public class ClienteController {
+
     private final ClientService clientService;
-    private ClientRepository clientRepository;
+
+    @Autowired
+    private ClientRepository repository;
+
+    @GetMapping("/clientes")
+    public List<Client> getAllClient(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteResponseDTO> getClienteById(@PathVariable Long id) {
+        Optional<Client> optionalClient = repository.findById(id);
+        if (optionalClient.isPresent()) {
+            return ResponseEntity.ok(new ClienteResponseDTO().convert(optionalClient.get()));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     private ClienteResponseDTO register(Client client) {
-        Client cliente = clientRepository.save(client);
+        Client cliente = repository.save(client);
         ClienteResponseDTO clienteResponseDTO = new ClienteResponseDTO();
         return clienteResponseDTO.convert(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClienteResponseDTO> updateCliente(@PathVariable Long id, @RequestBody Client updatedClient) {
+        Optional<Client> optionalClient = repository.findById(id);
+        if (optionalClient.isPresent()) {
+            Client client = optionalClient.get();
+            client.setNome(updatedClient.getNome());
+            client.setEmail(updatedClient.getEmail());
+            client.setCpf(updatedClient.getCpf());
+            Client savedClient = repository.save(client);
+            return ResponseEntity.ok(new ClienteResponseDTO().convert(savedClient));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCliente(@PathVariable Long id) {
+        Optional<Client> optionalClient = repository.findById(id);
+        if (optionalClient.isPresent()) {
+            repository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
